deb5749931dfad482c7b53be8dcc5d5ae2657192
Chukwuebuka <emmanuelmrchukwuebuka@gmail.com>
2025-01-08 15:16:54 +0100

    updated live chat

--- wp-content/plugins/estatik/includes/classes/class-assets-init.php
+++ wp-content/plugins/estatik/includes/classes/class-assets-init.php
@@ -190,7 +190,8 @@ class Es_Assets {
 			if (! empty( $_SERVER["REMOTE_ADDR"] ) ) {
 				$ip = $_SERVER["REMOTE_ADDR"];
 				if (! empty( $ip ) ) {
-					$xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=".$ip);
+					$geo_data = null; // Bypass geolocation for localhost
+					// $xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=".$ip);
 					if (! empty ( $xml->geoplugin_countryCode ) ) {
 						$localize['settings']['country'] = $xml->geoplugin_countryCode;
 					}
